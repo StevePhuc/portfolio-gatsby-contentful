9577320dc3701d2d6af065dbc2cf3515d9c1e6cc
Steve Phuc <stevephuc.dev@gmail.com>
2019-09-16 08:26:51 +0300

    update package , remove semantic

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import React from "react";
-import { Link } from "gatsby";
-import { Button } from "semantic-ui-react";
+// import { Link } from "gatsby";
+// import { Button } from "semantic-ui-react";
 
 import SEO from "../components/seo";
-import "semantic-ui-css/semantic.min.css";
+// import "semantic-ui-css/semantic.min.css";
 import "../components/index.css";
 import Header from "../components/header";
 import OldIndex from "../components/old-html";
